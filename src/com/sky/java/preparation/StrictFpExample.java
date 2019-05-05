f76679edb4bdb6325a9b21572303c476d3f0cd65
sumitkumar2019 <skumar2019@outlook.com>
2019-05-04 17:39:34 -0700

    strictfp: precision should not differ from platform to platform for floating point/Used with class, method and interface

---
+++ src/com/sky/java/preparation/StrictFpExample.java
@@ -0,0 +1,16 @@
+package com.sky.java.preparation;
+
+strictfp class NormalCalculation{
+    strictfp double calculateMe(){
+        double num1 = 10e+102;
+        double num2 = 6e+08;
+        return num1+num2;
+    }
+
+}
+public class StrictFpExample {
+    public static void main(String[] args) {
+        NormalCalculation normalCalculation = new NormalCalculation();
+        System.out.println(normalCalculation.calculateMe()); //strictfp:precision should not differ from platform to platform
+    }
+}
