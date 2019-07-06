6a79aa8581320d5cdef839885f6aaa6e7ab0d875
sumitkumar2019 <skumar2019@outlook.com>
2019-07-06 14:25:48 -0700

    Serialization

---
+++ src/com/sky/java/preparation/Serialization/SerializationExample.java
@@ -0,0 +1,39 @@
+package com.sky.java.preparation.Serialization;
+
+import com.sky.java.preparation.Serialization.object.Person;
+
+import java.io.*;
+
+public class SerializationExample {
+    public static void main(String[] args) {
+        Person person = new Person();
+        person.setId(1);
+        person.setName("Sumit");
+        person.setCity("ABC");
+
+        try (FileOutputStream fileOutputStream = new FileOutputStream("people.bin")) {
+            ObjectOutputStream outputStream = new ObjectOutputStream(fileOutputStream);
+            outputStream.writeObject(person);
+            outputStream.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try (FileInputStream fileInputStream = new FileInputStream("people.bin")) {
+            ObjectInputStream inputStream = new ObjectInputStream(fileInputStream);
+            Person person1= (Person)inputStream.readObject();
+            System.out.println(person1.getId());
+            System.out.println(person1.getName());
+            System.out.println(person1.getCity());
+            inputStream.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/com/sky/java/preparation/Serialization/object/Person.java
@@ -0,0 +1,33 @@
+package com.sky.java.preparation.Serialization.object;
+
+import java.io.Serializable;
+
+public class Person implements Serializable {
+    transient Integer id;
+    String name;
+    String city;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+}
