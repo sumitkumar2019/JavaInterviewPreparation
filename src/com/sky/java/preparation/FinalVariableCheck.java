fd68621fa3e2a1e56cb3864924a95f5b1708fb68
sumitkumar2019 <skumar2019@outlook.com>
2019-07-07 00:51:17 -0700

    Final variable check

---
+++ src/com/sky/java/preparation/FinalVariableCheck.java
@@ -0,0 +1,14 @@
+package com.sky.java.preparation;
+
+public final class FinalVariableCheck {
+    //final int x; //Compile time Error here
+
+    //Not allowed
+    /*public FinalVariableCheck(){
+        this.x=x; //Compile time Error here
+    }*/
+    //Not allowed : //Compile time Error here
+    /*public void setX(int x) {
+         this.x = x;
+    }*/
+}
